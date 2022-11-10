7302b270853273e4e44a4d48e839ef327dfbefba
Marianoq513 <marianoq513@gmail.com>
2022-11-09 21:45:06 -0300

    agrego un carrito con array

--- app.js
+++ app.js
@@ -13,81 +13,97 @@ function cant(precio, corte) {
     cant(precio, corte);
   }
 }
-function multi(cantidad, precio, corte) {
-  let total = 0;
-  total = cantidad * precio;
-  alert(
-    "Usted compro " +
-      cantidad +
-      " kg " +
-      " de " +
-      corte +
-      " y el total de su compra es de $" +
-      total
-  );
+const carrito = [];
+let total = 0;
+function multi(cantidad, precio) {
+  let subtotal = cantidad * precio;
+  total = total + subtotal;
 }
-
+function comprar() {
+  alert(
+    "Escoja el corte que desea llevar indicando el numero que tiene al comienzo "
+  );
+  let corte = 0;
+  while (
+    corte != "1" &&
+    corte != "2" &&
+    corte != "3" &&
+    corte != "4" &&
+    corte != "5"
+  ) {
+    corte = prompt(
+      "1 Vacio $1.400xkg " +
+        "2 Costillas $800xkg " +
+        "3 cuadrada $1.500xkg " +
+        "4 Osobuco $700xkg " +
+        "5 Carne molida $700xkg"
+    );
+    switch (corte) {
+      case "1":
+        cant(1400, "vacio");
+        break;
+      case "2":
+        cant(800, "costillas");
+        break;
+      case "3":
+        cant(1500, "cuadrada");
+        break;
+      case "4":
+        cant(700, "osobuco");
+        break;
+      case "5":
+        cant(700, "carne molida");
+        break;
+      default:
+        alert("Numero incorrecto ingrese uno de la tabla");
+        break;
+    }
+  }
+}
 let nombre = prompt("Ingrese su nombre");
 saludar();
-alert(
-  "Escoja el corte que desea llevar indicando el numero que tiene al comienzo "
+comprar();
+let seguirComprando = prompt(
+  "Si quiere seguir comprando ingrese 1 sino cualquier otra tecla"
 );
-let corte = 0;
-while (
-  corte != "1" &&
-  corte != "2" &&
-  corte != "3" &&
-  corte != "4" &&
-  corte != "5"
-) {
-  corte = prompt(
-    "1 Vacio $1.400xkg " +
-      "2 Costillas $800xkg " +
-      "3 cuadrada $1.500xkg " +
-      "4 Osobuco $700xkg " +
-      "5 Carne molida $700xkg"
+do {
+  comprar();
+  seguirComprando = prompt(
+    "Si quiere seguir comprando ingrese 1 sino cualquier otra tecla"
   );
-  switch (corte) {
-    case "1":
-      cant(1400, "vacio");
-      break;
-    case "2":
-      cant(800, "costillas");
-      break;
-    case "3":
-      cant(1500, "cuadrada");
-      break;
-    case "4":
-      cant(700, "osobuco");
-      break;
-    case "5":
-      cant(700, "carne molida");
-      break;
-    default:
-      alert("Numero incorrecto ingrese uno de la tabla");
-      break;
-  }
-}
-class Cortes {
-  constructor(tipo, utilidad, precio) {
-    this.tipo = tipo;
-    this.utilidad = utilidad;
-    this.precio = precio;
-  }
-  mostrarUtilidad() {
-    console.log(this.utilidad);
-  }
-}
-const vacio = new Cortes("sin hueso", "asado", 1400);
-const costilla = new Cortes("con hueso", "asado", 800);
-const cuadrada = new Cortes("sin hueso", "bifes", 1500);
-const osobuco = new Cortes("con hueso", "olla", 700);
-const carneMolida = new Cortes("sin hueso", "hamburgesas", 700);
-console.log(vacio);
-console.log(costilla);
-console.log(cuadrada);
-console.log(osobuco);
-console.log(carneMolida);
-vacio.mostrarUtilidad();
-despedir();
+} while (seguirComprando == 1);
 
+const cortes = [
+  {
+    corte: "Vacio",
+    utilidad: "Asado",
+    precio: 1400,
+  },
+  {
+    corte: "Costillas",
+    utilidad: "Asado",
+    precio: 800,
+  },
+  {
+    corte: "Cuadrada",
+    utilidad: "Blanda",
+    precio: 1500,
+  },
+  {
+    corte: "Osobuco",
+    utilidad: "Olla",
+    precio: 700,
+  },
+  {
+    corte: "Carne molida",
+    utilidad: "Hamburgesas",
+    precio: 700,
+  },
+];
+
+for (const corte of cortes) {
+  console.log(corte);
+}
+carrito.push(total);
+alert("El total de su compra es " + carrito);
+despedir();
