efa85aba076b2318f5aeb880e544a1a4518505c2
Marianoq513 <marianoq513@gmail.com>
2022-12-14 21:50:49 -0300

    Carrito

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+//Variables
+//Selectores
+//Fuciones
+//Listeners
+//Ejecuciones
+
 //DOM
 //FOREACH PARA CREAR LAS TARJETAS DE LOS CORTES DE ASADO
+
 productosA.forEach((productosA) => {
   const nuevaCardA = document.createElement("div");
   nuevaCardA.className = "tarjetaProducto";
@@ -7,8 +14,8 @@ productosA.forEach((productosA) => {
   <img src=${productosA.imgSrc}>
   <p> ${productosA.utilidad} </p>
   <p>$${productosA.precio}</p>
-  <a href="../assets/img/puntadeespalda1.jpg">Comprar</a>
- `;
+  <button class="buttonCompra" data-id="${productosA.id}">Añadir al carrito</button>
+`;
   asadoConteiner.append(nuevaCardA);
 });
 
@@ -22,8 +29,8 @@ productosB.forEach((productosB) => {
     <img src=${productosB.imgSrc}>
     <p> ${productosB.utilidad} </p>
     <p>$${productosB.precio}</p>
-    <a href="../assets/img/puntadeespalda1.jpg">Comprar</a>
-   `;
+    <button class="buttonCompra" data-id="${productosB.id}">Añadir al carrito</button>
+    `;
   blandaConteiner.append(nuevaCardB);
 });
 
@@ -37,8 +44,8 @@ productosC.forEach((productosC) => {
       <img src=${productosC.imgSrc}>
       <p> ${productosC.utilidad} </p>
       <p>$${productosC.precio}</p>
-      <a href="../assets/img/puntadeespalda1.jpg">Comprar</a>
-     `;
+      <button class="buttonCompra" data-id="${productosC.id}">Añadir al carrito</button>
+      `;
   embutidoConteiner.append(nuevaCardC);
 });
 
@@ -52,9 +59,41 @@ productosD.forEach((productosD) => {
         <img src=${productosD.imgSrc}>
         <p> ${productosD.utilidad} </p>
         <p>$${productosD.precio}</p>
-        <a href="../assets/img/puntadeespalda1.jpg">Comprar</a>
-       `;
+        <button class="buttonCompra" data-id="${productosD.id}">Añadir al carrito</button>
+        `;
   achurasConteiner.append(nuevaCardD);
 });
 
 //Fin forEach achuras
+//Funcion para agregar al carrito
+let carrito;
+if (localStorage.getItem("carrito")) {
+  carrito = JSON.parse(localStorage.getItem("carrito"));
+} else {
+  carrito = [];
+}
+console.log("carrito", carrito);
+const añadirAlCarrito = (e) => {
+  const idProductoElegido = e.target.getAttribute("data-id");
+  const productoElegido = productosA.find(
+    (productosA) => productosA.id == idProductoElegido
+  );
+  carrito.push(productoElegido);
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  console.log(carrito);
+};
+
+//Boton de compra
+const buttonCompra = document.querySelectorAll(".buttonCompra");
+buttonCompra.forEach((button) => {
+  button.addEventListener("click", añadirAlCarrito);
+});
+
+//Barra buscador
+const searchBarInput = document.querySelector("#searchBarInput");
+
+const searchBarButton = document.querySelector("#searchBarButton");
+
+searchBarButton.addEventListener("click", () => {
+  console.log(searchBarInput.value);
+});
--- js/data.js
+++ js/data.js
@@ -2,7 +2,8 @@
 //DOM
 //Objeto constructor Asado
 class Asado {
-  constructor(corte, imgSrc, utilidad, precio) {
+  constructor(id, corte, imgSrc, utilidad, precio) {
+    this.id = id;
     this.corte = corte;
     this.imgSrc = imgSrc;
     this.utilidad = utilidad;
@@ -12,30 +13,35 @@ class Asado {
 
 //lISTA DE CORTES DE ASADO
 const asado1 = new Asado(
+  1,
   "Punta de espalda",
   (imgsrc = "../assets/img/puntadeespalda1.jpg"),
   "asado",
   1300
 );
 const asado2 = new Asado(
+  2,
   "Vacio",
   (imgSrc = "../assets/img/vacio1.jpg"),
   "Asado",
   1400
 );
 const asado3 = new Asado(
+  3,
   "Tapa de asado",
   (imgSrc = "../assets/img/tapadeasado1.jpg"),
   "Asado",
   1300
 );
 const asado4 = new Asado(
+  4,
   "Costillas",
   (imgSrc = "../assets/img/costillas1.jpg"),
   "Asado",
   900
 );
 const asado5 = new Asado(
+  5,
   "Entraña",
   (Image = "../assets/img/entrana1.jpg"),
   "Asado",
@@ -48,7 +54,8 @@ const asadoConteiner = document.getElementById("asadoConteiner");
 
 //Objeto constructor Blandas
 class Blanda {
-  constructor(corte, imgSrc, utilidad, precio) {
+  constructor(id, corte, imgSrc, utilidad, precio) {
+    this.id = id;
     this.corte = corte;
     this.imgSrc = imgSrc;
     this.utilidad = utilidad;
@@ -58,36 +65,42 @@ class Blanda {
 
 //lISTA DE CORTES DE ASADO
 const blanda1 = new Blanda(
+  6,
   "Costeleta",
   (imgsrc = "../../assets/img/costeleta1.jpeg"),
   "Blanda",
   1100
 );
 const blanda2 = new Blanda(
+  7,
   "Cuadrada",
   (imgSrc = "../assets/img/cuadrada1.jpg"),
   "Blanda",
   1400
 );
 const blanda3 = new Blanda(
+  8,
   "Cuadril",
   (imgSrc = "../assets/img/cuadril1.jpg"),
   "Blanda",
   1250
 );
 const blanda4 = new Blanda(
+  9,
   "Nalga",
   (imgSrc = "../assets/img/nalga1.png"),
   "Blanda",
   1400
 );
 const blanda5 = new Blanda(
+  10,
   "Punta de pierna",
   (Image = "../assets/img/puntadepierna1.jpg"),
   "Blanda",
   1200
 );
 const blanda6 = new Blanda(
+  11,
   "Peceto",
   (Image = "../assets/img/peceto1.png"),
   "Blanda",
@@ -100,7 +113,8 @@ const blandaConteiner = document.getElementById("blandaConteiner");
 
 //Objeto constructor Embutidos
 class Embutido {
-  constructor(corte, imgSrc, utilidad, precio) {
+  constructor(id, corte, imgSrc, utilidad, precio) {
+    this.id = id;
     this.corte = corte;
     this.imgSrc = imgSrc;
     this.utilidad = utilidad;
@@ -110,18 +124,21 @@ class Embutido {
 
 //lISTA DE EMBUTIDOS
 const embutido1 = new Embutido(
+  12,
   "Chorizo",
   (imgsrc = "../assets/img/chorizo1.jpg"),
   "Embutido",
   1000
 );
 const embutido2 = new Embutido(
+  13,
   "Morcilla",
   (imgSrc = "../assets/img/morcilla1.jpg"),
   "Embutido",
   800
 );
 const embutido3 = new Embutido(
+  14,
   "Salame",
   (imgSrc = "../assets/img/salame1.jpg"),
   "Embutido",
@@ -134,7 +151,8 @@ const embutidoConteiner = document.getElementById("embutidoConteiner");
 
 //Objeto constructor achuras
 class Achuras {
-  constructor(corte, imgSrc, utilidad, precio) {
+  constructor(id, corte, imgSrc, utilidad, precio) {
+    this.id = id;
     this.corte = corte;
     this.imgSrc = imgSrc;
     this.utilidad = utilidad;
@@ -144,24 +162,28 @@ class Achuras {
 
 //lISTA DE ACHURAS
 const achura1 = new Achuras(
+  15,
   "Chinchulin",
   (imgsrc = "../assets/img/chinchulin1.jpg"),
   "Achura",
   1000
 );
 const achura2 = new Achuras(
+  16,
   "Molleja",
   (imgSrc = "../assets/img/molleja1.jpg"),
   "Achura",
   700
 );
 const achura3 = new Achuras(
+  17,
   "Seso",
   (imgSrc = "../assets/img/seso1.jpg"),
   "Achura",
   700
 );
 const achura4 = new Achuras(
+  18,
   "Riñon",
   (imgSrc = "../assets/img/riñon1.jpg"),
   "Achura",
